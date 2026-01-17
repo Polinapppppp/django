3caf4ff5b065d2d714594d85d7bcea1c0f81bd40
Polinapppppp <ppppppvoha@gmail.com>
2026-01-17 19:04:04 +0300

    m10

---
+++ brunoyam_news/news/urls.py
@@ -0,0 +1,14 @@
+# news/urls.py
+from django.urls import path
+from . import views  # импорт views из текущего приложения
+
+# УДАЛИТЕ эту строку - она вызывает циклический импорт!
+# from brunoyam_news.urls import urlpatterns
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('news/', views.index, name='index'),
+    path('modul10/practic/', views.practic, name='practic'),
+    path('modul10/z2/', views.z2, name='z2'),
+    path('modul10/z3/', views.z3, name='z3'),
+]
---
+++ brunoyam_news/news/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def index(request):
+    return render(request,'news/index.html')
+
+def practic(request):
+    return render(request, 'modul10/practic.html')
+def z2(request):
+    return render(request, 'modul10/z2.html')
+def z3(request):
+    return  render(request, 'modul10/z3.html')
