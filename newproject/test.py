77d2e67152d225381c2d912ba20be12a4fcf5a53
Polinapppppp <ppppppvoha@gmail.com>
2026-02-05 15:20:05 +0300

    sc

--- newproject/test.py
+++ newproject/test.py
@@ -1,32 +1,15 @@
 from django.test import TestCase
-from .models import Tag
-from .factories import ProductFactory
 from .models import Category, Product
+from .services import get_product_data
 
-class TagModelTest(TestCase):
-    def test_tag_creation(self):
-        tag = Tag.objects.create(name="Органический")
-        self.assertEqual(tag.name, "Органический")
-        self.assertIsInstance(tag, Tag)
+class ProductServiceTest(TestCase):
+    def test_get_product_data(self):
+        category = Category.objects.create(name="Фрукты")
+        Product.objects.create(name="Банан", price=45.00, category=category)
 
+        data = get_product_data()
 
-
-class ProductWithFixtureTest(TestCase):
-    fixtures = ['categories.json']
-
-    def test_product_with_fixture_category(self):
-        fruit_category = Category.objects.get(name="Фрукты")
-        product = Product.objects.create(
-            name="Апельсин",
-            price=70.00,
-            category=fruit_category
-        )
-        self.assertEqual(product.category.name, "Фрукты")
-
-class ProductWithFactoryTest(TestCase):
-    def test_product_creation_with_factory(self):
-        product = ProductFactory.create()
-        self.assertIsNotNone(product.name)
-        self.assertIsNotNone(product.price)
-        self.assertIsNotNone(product.category)
-        self.assertIsInstance(product.category, Category)
+        self.assertEqual(len(data['categories']), 1)
+        self.assertEqual(len(data['products']), 1)
+        self.assertEqual(data['products'][0].name, "Банан")
+        self.assertEqual(data['products'][0].category.name, "Фрукты")
