860c5bc96973f09e0e7593ae81a0a10fb0c4f07c
Polinapppppp <ppppppvoha@gmail.com>
2026-02-10 16:19:16 +0300

    lc

--- newproject/views.py
+++ newproject/views.py
@@ -1,26 +1,27 @@
 
-from django.views.generic import ListView, FormView, TemplateView
+from django.views.generic import ListView, CreateView, TemplateView
 from django.urls import reverse_lazy
-from .models import Product, Contact
+from .services import get_product_data
 from .forms import ContactForm
 
 class ProductListView(ListView):
-    model = Product
     template_name = 'product_list.html'
-    context_object_name = 'products'
     paginate_by = 5
 
     def get_queryset(self):
-        return Product.objects.select_related('category')
+        data = get_product_data()
+        return data['products']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        data = get_product_data()
+        context['categories'] = data['categories']
+        return context
 
 class ContactView(FormView):
     template_name = 'contact_form.html'
     form_class = ContactForm
     success_url = reverse_lazy('contact_success')
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
 
 class ContactSuccessView(TemplateView):
     template_name = 'contact_success.html'
